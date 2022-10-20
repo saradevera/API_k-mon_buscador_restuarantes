9a07b5bdac5d2da52a456e12ff1be298f3b2a8ba
Toni <107318721+Toni2Morales@users.noreply.github.com>
2022-10-20 18:27:25 +0200

    commit

--- api.py
+++ api.py
@@ -25,63 +25,63 @@ def hello():
 
 
 
-@app.route("/RecomendacionPorPreferencias", methods = ['POST'])
-def RecomendacionPorPreferencias():
-  pref = request.get_json()["preferencias"]
-  filtro = request.args.get("Filtro")
-  # NOS CONECTAMOS A LA DB PARA OBTENER LA TABLA DE DATOS
-  data = conn_db()
+# @app.route("/RecomendacionPorPreferencias", methods = ['POST'])
+# def RecomendacionPorPreferencias():
+#   pref = request.get_json()["preferencias"]
+#   filtro = request.args.get("Filtro")
+#   # NOS CONECTAMOS A LA DB PARA OBTENER LA TABLA DE DATOS
+#   data = conn_db()
 
-  data = info_from_type(data)
-  data = info_from_description(data)
-  data = new_columns(data)
-  data = columnas_sumatorio(data)
+#   data = info_from_type(data)
+#   data = info_from_description(data)
+#   data = new_columns(data)
+#   data = columnas_sumatorio(data)
 
-  if filtro == "restaurantes":
-    data = data[data["resotie"] == "Restaurante"]
-  elif filtro == "tiendas":
-    data = data[data["resotie"] == "Tienda"]
-  elif filtro == "todos":
-    pass
-  # RECIBIMOS LA INFO DE LA API DE NUESTRA WEB
-  recomendaciones = preferencias(pref, data)
-  recomendacionesDef= [int(x) for x in recomendaciones]
-  return str(recomendacionesDef)[1:-1]
+#   if filtro == "restaurantes":
+#     data = data[data["resotie"] == "Restaurante"]
+#   elif filtro == "tiendas":
+#     data = data[data["resotie"] == "Tienda"]
+#   elif filtro == "todos":
+#     pass
+#   # RECIBIMOS LA INFO DE LA API DE NUESTRA WEB
+#   recomendaciones = preferencias(pref, data)
+#   recomendacionesDef= [int(x) for x in recomendaciones]
+#   return str(recomendacionesDef)[1:-1]
 
 
 
-@app.route("/RecomendacionDependiente", methods = ['GET'])
+# @app.route("/RecomendacionDependiente", methods = ['GET'])
 
-def RecomendacionDependiente():
-  filtro = request.args.get("Filtro")
-  ID = int(request.args.get("ID"))
-    # NOS CONECTAMOS A LA DB PARA OBTENER LA TABLA DE DATOS
-  data = conn_db()
-  data = info_from_type(data)
-  data = info_from_description(data)
-  data = new_columns(data)
-  data = columnas_sumatorio(data)
-  if filtro == "restaurantes":
-    data = data[data["resotie"] == "Restaurante"]
-  elif filtro == "tiendas":
-    data = data[data["resotie"] == "Tienda"]
-  elif filtro == "todos":
-    pass
-  # ELIMINAMOS LAS COLUMNAS QUE NO SON NECESARIAS EN ESTA FASE
-  index = [x for x in data["index"]]
-  indice_matriz = index.index(ID)
-  data.drop(columns=['place_name', 'type', 'resotie',
-                              'place_id', 'address', 'phone',
-                              'website', 'description', 'photos_link',
-                              'thumbnail', 'is_spain', 'keywords', 'localidad', "index"], inplace = True)
+# def RecomendacionDependiente():
+#   filtro = request.args.get("Filtro")
+#   ID = int(request.args.get("ID"))
+#     # NOS CONECTAMOS A LA DB PARA OBTENER LA TABLA DE DATOS
+#   data = conn_db()
+#   data = info_from_type(data)
+#   data = info_from_description(data)
+#   data = new_columns(data)
+#   data = columnas_sumatorio(data)
+#   if filtro == "restaurantes":
+#     data = data[data["resotie"] == "Restaurante"]
+#   elif filtro == "tiendas":
+#     data = data[data["resotie"] == "Tienda"]
+#   elif filtro == "todos":
+#     pass
+#   # ELIMINAMOS LAS COLUMNAS QUE NO SON NECESARIAS EN ESTA FASE
+#   index = [x for x in data["index"]]
+#   indice_matriz = index.index(ID)
+#   data.drop(columns=['place_name', 'type', 'resotie',
+#                               'place_id', 'address', 'phone',
+#                               'website', 'description', 'photos_link',
+#                               'thumbnail', 'is_spain', 'keywords', 'localidad', "index"], inplace = True)
   
-  # ESCALAMOS LOS DATOS Y CREAMOS LA MATRIZ
-  ss = StandardScaler()
-  data_scalado = ss.fit_transform(data)
-  # CREAMOS EL COMPARATIVO DE DATOS Y EL INDICE QUE SE CONSULTARÁ
-  # GENERAMOS LA SIMILITUD COSENO Y SE ORDENA SEGÚN PARECIDOS
-  recomendaciones = get_recommendations(data_scalado, indice_matriz)
-  return str(recomendaciones)[1:-1]
+#   # ESCALAMOS LOS DATOS Y CREAMOS LA MATRIZ
+#   ss = StandardScaler()
+#   data_scalado = ss.fit_transform(data)
+#   # CREAMOS EL COMPARATIVO DE DATOS Y EL INDICE QUE SE CONSULTARÁ
+#   # GENERAMOS LA SIMILITUD COSENO Y SE ORDENA SEGÚN PARECIDOS
+#   recomendaciones = get_recommendations(data_scalado, indice_matriz)
+#   return str(recomendaciones)[1:-1]
 
 
 if __name__ == '__main__':
